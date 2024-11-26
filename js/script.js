2cd57cb65472636c8a2b47cb45d1d8f633b795b5
Olivier Obielak <129651282+OlivierObielak@users.noreply.github.com>
2024-11-26 21:08:26 +0000

    added logo

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 document.addEventListener("keydown", event => {
     const letterPressed = event.key.toLowerCase();
     const gifContainer = document.getElementById("lettergif-container");
-    const instructionDiv = document.getElementById("instruction");
 
     if (letterPressed >= `a` && letterPressed <= `z`) {
         const gifDisplayed = `letters/${letterPressed}.gif`;
         const soundPlayed = `sounds/${letterPressed}.mp3`;
-        instructionDiv.style.display = "none";
+        
 
 
         gifContainer.innerHTML = `<img src="${gifDisplayed}" alt="${letterPressed} GIF">`;
